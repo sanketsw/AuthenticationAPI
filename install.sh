fa9bf28fc96eaaf2d429448256c9934cc78dd38e
edwinquaihoi <edwinquaihoi@gmail.com>
2016-04-29 16:34:32 +1000

    Initial commit of install script

---
+++ install.sh
@@ -0,0 +1,11 @@
+#
+
+# inputs management server url, username, password, target catalogue
+
+# Log into Management Console
+apic login
+
+# Push Product/API into Drafts
+
+# Publish API to Target Catalogue 
+apic publish product.yaml
